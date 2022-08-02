2093d82a1bf7830909721f3f58aa76cff66d24bb
Claudia Neme <clau.neme@gmail.com>
2022-08-02 17:45:21 -0300

    Semana 07 de Folcademy

--- config/firebase.js
+++ config/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIRBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  //datos de configuracion de Firebase
 };
 
 const app = firebase.initializeApp(firebaseConfig);

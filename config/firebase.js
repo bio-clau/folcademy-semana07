2aae5ed7eba96d20dc11658ca2fc96e240d539c9
bio-clau <87905574+bio-clau@users.noreply.github.com>
2022-08-02 16:52:08 -0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,1 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBkWfIIrT2qzs5lyoIIgJOSHd_zfwghB0o",
-  authDomain: "next-auth-4617f.firebaseapp.com",
-  projectId: "next-auth-4617f",
-  storageBucket: "next-auth-4617f.appspot.com",
-  messagingSenderId: "194010764940",
-  appId: "1:194010764940:web:3a88a6470bff8d9f5ccda9",
-};
-
-const app = firebase.initializeApp(firebaseConfig);
-export const auth = app.auth();
+//Aqui debe ir la configuracion de Firebase
